3798913b30777262007ef8eef676c27bd7bbed31
Mo Abukar <mo-abukar10@hotmail.com>
2026-04-13 23:20:34 +0100

    docs: add Terraform example for public IP addresses

---
+++ examples/terraform/publicip.tf
@@ -0,0 +1,11 @@
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  tags = {
+    environment = "dev"
+  }
+}
